532fafc7813b4b0961a5b51f41801a5e973fb786
Sobiech <mikolajmikolaj090302@gmail.com>
2023-04-05 10:13:33 +0200

    Zmiana przykładu

--- src/W04/Z04/Main.java
+++ src/W04/Z04/Main.java
@@ -6,8 +6,9 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         new MyFrame(75);
-        new Circle(100,100,5,0);
-        new Circle(600,100,-5,0);
+        for (int i = 0; i < 40; i++) {
+            new Circle();
+        }
 
         MyFrame.self.animate();
     }
