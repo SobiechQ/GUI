30447b705ce14b9b11b75b331067d6bea2bb9918
Mikołaj <mikolajmikolaj090302@gmail.com>
2023-04-11 16:41:14 +0200

    reverse String stream

---
+++ src/W05/Z06/Main.java
@@ -0,0 +1,32 @@
+package W05.Z06;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        Stream.of(5,10,15,20,25,30)
+                .mapToInt(i -> i)
+                .filter(i -> i<15)
+                .flatMap(i->
+                        Stream.generate(()->i)
+                                .mapToInt(j->j)
+                                .limit(i))
+                .forEach(System.out::println);
+        String[] input = {"Jeden", "Dwa", "Trzy", "Cztery"};
+        Stream.of(input)
+                .map(s->{
+                    StringBuilder output = new StringBuilder();
+                    for (int i = s.length()-1; i >= 0; i--)
+                        output.append(s.charAt(i));
+                    return output;
+                })
+                .forEach(System.out::println);
+
+
+
+
+
+    }
+}
