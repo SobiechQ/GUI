2a23b025e8fa7a2616f21cdb6fa36f5572a88278
Mikołaj <mikolajmikolaj090302@gmail.com>
2023-04-13 13:45:20 +0200

    Thread

---
+++ src/W06/Z02/Main.java
@@ -0,0 +1,51 @@
+package W06.Z02;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    /*
+    public static double foo(double input){
+        double output = 10;
+        for (double i = 0; i < input; i+=1d/3d) {
+            output *=  1 + Math.random()*10;
+        }
+        return output;
+    }
+    private class MyMath{
+        public double foo(){
+            return 0l
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+        t1.start();
+
+        System.out.println("raz");
+        System.out.println(t1.getState());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("dwa");
+        while (t1.getState()== Thread.State.RUNNABLE)
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        System.out.println("No to sie zakonczylo");
+
+
+    }
+    */
+}
